b3cb2b520b5a43e195db0cf3756faec0ee0a19ab
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-10-30 02:16:27 -0400

    prevent default and check for selected radio value on submit

--- static/trivia.js
+++ static/trivia.js
@@ -8,16 +8,22 @@ $.get('/questions.json', (data) => {
     data.forEach(d => {
         displayQuestion(d)
         displayAnswers(d)
-        
-        
+        $('#questionForm').on('submit', (event) => {
+            event.preventDefault();
+            const checked = $('input[name=answer]:checked').val()
+            console.log(`This value: ${checked}`)
+        })  
     })
 });
+
+
 //  render question 
 function displayQuestion(dictionary) {
-    const question = document.createElement('div');
-    question.setAttribute('class', 'question');
-    question.textContent = dictionary['question'];
-    $('#questions').append(question)
+    $('#question').text(dictionary['question'])
+    // const question = document.createElement('div');
+    // question.setAttribute('class', 'question');
+    // question.textContent = dictionary['question'];
+    // $('#questions').append(question)
 };
 // compile answer options from incorrect and correct
 function compileAnswers(dictionary)  {
@@ -47,12 +53,31 @@ function randomChoice(answerArray) {
 // render answers for question
 function displayAnswers(dictionary) {
     const answers = randomChoice(compileAnswers(dictionary))
-    console.log(answers)
-    answers.forEach(a => {
-        const answer = document.createElement('div')
-        answer.setAttribute('class', 'answer')
-        answer.textContent = a;
-        $('#questions').append(answer)
-    })
+    // console.log(answers)
+    // answers.forEach(a => {
+    //     const answer = document.createElement('div')
+    //     answer.setAttribute('class', 'answer')
+    //     answer.textContent = a;
+    //     $('#questions').append(answer)   
+    // })
+    $('#answerA').text(answers[0])
+    $('#answerB').text(answers[1])
+    $('#answerC').text(answers[2])
+    $('#answerD').text(answers[3])
+
 }
 
+function checkAnswer(form, name) {
+    console.log(form)
+    console.log(name)
+    const radios = form.elements[name];
+    console.log(radios)
+    radios.forEach(r => {
+        if ( r.checked ) {
+            const userAnswer = r.value
+            console.log(`radio for loop answer: ${userAnswer}`)
+            return r.value
+        }
+        
+    })
+}
