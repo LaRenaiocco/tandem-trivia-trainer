dd6abcfca0d5eaf0dd27dcea8fe9cd7f3112ab41
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-10-29 20:54:29 -0400

    helper.py added, renamed data file

---
+++ helper.py
@@ -0,0 +1,20 @@
+"""Helper functions for flask server"""
+import json
+from random import randint
+
+data = open('data/tandem_data.json')
+trivia_data = json.loads(data.read())
+
+def select_ten():
+    """return a set of 10 random numbers for question selection"""
+    random_10 = set([])
+    while len(random_10) < 11:
+        num = randint(0,20)
+        random_10.add(num)
+    
+    return random_10
+
+def generate_questions(random_10):
+    """Parses 10 questions from trivia_data"""
+
+
